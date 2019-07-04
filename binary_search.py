887d3e245175a940088de99f2fdda2f5df6d1f32
Mikhail Gusarov <vgg639@gmail.com>
2019-07-04 16:49:59 +0300

    Немного исправил

--- binary_search.py
+++ binary_search.py
@@ -3,14 +3,14 @@ def binary_search(arr, val)-> int:
     low = 0
     high = len(arr) - 1
     
-    mid = int((low + high) / 2)
-    guess = arr[mid]
-    while guess != val:
+    while low <= high:
         mid = int((low + high) / 2)
         guess = arr[mid]
         if guess < val:
             low = mid + 1
         elif guess > val:
             high = mid - 1
-    return mid
+        else:
+            return mid
+    return
     
