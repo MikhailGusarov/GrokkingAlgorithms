770b2ed8f65512936c6f9b6ee4376d0eb1db5bd9
MikhailGusarov <vgg639@gmail.com>
2019-07-06 00:54:24 +0300

    Выборочная сортировка

---
+++ sort_select.py
@@ -0,0 +1,18 @@
+def find_smallest(arr):
+    smallest = arr[0]
+    small_index = 0
+    for i in range(1, len(arr)):
+        if smallest > arr[i]:
+            smallest = arr[i]
+            small_index = i
+    return small_index
+
+
+def selection_sort(arr):
+    sort_arr = []
+    for i in range(0, len(arr)):
+        sort_arr.append(arr.pop(find_smallest(arr)))
+    return sort_arr
+
+
+print(selection_sort([5, 4, 11, 21, 2, -5, 5]))
