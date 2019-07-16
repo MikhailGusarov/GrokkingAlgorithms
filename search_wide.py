748282388eca019f37c7588a058f96ba761d50dd
MikhailGusarov <vgg639@gmail.com>
2019-07-16 23:14:13 +0300

    Комменты добавил

--- search_wide.py
+++ search_wide.py
@@ -1,14 +1,15 @@
-def search_wide(graph, start, end):
-    arr = [start]
-    arr_search = []
+def search_wide(graph: dict, start: str, end: str) -> str:
+    """Поиск в ширину"""
+    arr = [start]  # очередь
+    arr_search = []  # список просмотренных вершин графа
     while arr:
-        step = arr.pop(0)
+        step = arr.pop(0)  # взять из очереди первый элемент
         if step == end:
             return 'Е бой'
-        if step not in arr_search:
-            arr_search.append(step)
-            arr += graph[step]
-    return 'Говно'
+        if step not in arr_search:  # если элемента в arr_search нет
+            arr_search.append(step)  # добавить эллемент в список
+            arr += graph[step]  # добавить всех соседей в очередь
+    return 'Говно'  # если очередь опустела, то пути нет
 
 
 graph_initial = {
