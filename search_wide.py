59d4cb9294d90822a437e183e8d9130c13590244
MikhailGusarov <vgg639@gmail.com>
2019-07-16 22:26:49 +0300

    Сделяль по PEP8

--- search_wide.py
+++ search_wide.py
@@ -1,22 +1,22 @@
-def search_wide(graph,start,end):
-    arr=[start]
-    arrSearch = []
+def search_wide(graph, start, end):
+    arr = [start]
+    arr_search = []
     while arr:
         step = arr.pop(0)
         if step == end:
             return 'Е бой'
-        if step not in arrSearch:
-            arrSearch.append(step)
+        if step not in arr_search:
+            arr_search.append(step)
             arr += graph[step]
     return 'Говно'
 
-graph = {'A': ['B', 'F'],
-         'B':['A','F','C'],
-         'C':['B','D'],
-         'D':['C','E'],
-         'E':['F','D'],
-         'F':['A','B','G','E'],
-         'G':['F','H'],
-         'H':['G']}
 
-
+graph_initial = {
+    'A': ['B', 'F'],
+    'B': ['A', 'F', 'C'],
+    'C': ['B', 'D'],
+    'D': ['C', 'E'],
+    'E': ['F', 'D'],
+    'F': ['A', 'B', 'G', 'E'],
+    'G': ['F', 'H'],
+    'H': ['G']}
