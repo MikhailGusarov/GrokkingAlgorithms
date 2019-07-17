228da357a751be70a8e39e282a9a35cb6773552e
Mikhail Gusarov <vgg639@gmail.com>
2019-07-17 16:45:26 +0300

    Алгоритм Дейкстры (заготовка №2)

--- algorithm_dijkstra.py
+++ algorithm_dijkstra.py
@@ -1,13 +1,16 @@
-def algorithm_dijkstra(graph: dict,costs: dict,parent: dict, start: str, end: str)-> str:
+def algorithm_dijkstra(graph: dict, start: str, end: str)-> str:
     """Алгоритм Дейкстры"""
-    processed = []
-    pass
+    processed = []  # проверенные вершины
+    costs = graph[start].copy()  # цены на тек. проверке
+    parent = {i: start for i in graph[start].keys()}  # родители на тек. проверку
+    if end not in costs.keys():
+        costs[end] = float("inf")
+        parent[end] = None
+
 
 graph = {'S': {'A': 6, 'B': 2},
          'A': {'E': 1},
          'B': {'A': 3, 'E': 5},
          'E': {}}
 
-costs = {'A': 6, 'B': 2, 'E': float("inf")}
-
-parent = {'A': 'S', 'B': 'S': None}
+algorithm_dijkstra(graph, 'S', 'E')
