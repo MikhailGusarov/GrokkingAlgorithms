32c4a0dcc61a745c08f389ebd4ec291b385b1ceb
MikhailGusarov <vgg639@gmail.com>
2019-07-30 19:21:34 +0300

    Алгортм дейкстры. Добавил функцию поиска шага

--- algorithm_dijkstra.py
+++ algorithm_dijkstra.py
@@ -6,6 +6,17 @@ def algorithm_dijkstra(graph: dict, start: str, end: str) -> None:
     if end not in costs.keys():
         costs[end] = float("inf")
         parent[end] = None
+    min_in_dict(costs, processed)
+
+
+def min_in_dict(costs: dict, processed: list) -> str:
+    min_val = float('inf')
+    for k, v in costs.items():
+        if k not in processed and v < min_val:
+            res = k
+            min_val = v
+    return res
+
 
 
 graph1 = {'S': {'A': 6, 'B': 2},
